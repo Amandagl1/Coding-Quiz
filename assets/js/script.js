145739d5d8a985a452b1f269dd467f54a0f6d4b4
Amanda Lee <leeandama87@gmail.com>
2022-10-25 16:18:01 -0700

    Added text content, id tags, and class tags in html. Added styling in css.

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var styleOl = document.querySelectorAll("li");
+// styleOl.style.color = "rgb(137, 6, 177)";
+
+for (var i = 0; i < styleOl.length; i++);
+styleOl[i].setAttribute("style", "color: rgb(137, 6, 177)");
+
+
+var firstChildOl = document.getElementById("first-child-ol");
