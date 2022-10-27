a8b9c73f52b4de5fea735119bb4c7c214200a9a7
Amanda Lee <leeandama87@gmail.com>
2022-10-27 13:18:19 -0700

    Created event listener that creates the list element. Styled in css so the width of the content is pushed to the center.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,51 +7,99 @@ var secondsLeft = 75;
 function setTime() {
     var timerInterval = setInterval(function() {
         secondsLeft--;
-        timeEl.textContent = secondsLeft + "remaining";
+        timeEl.textContent = "Time: " + secondsLeft;
     
         if(secondsLeft === 0) {
         clearInterval(timerInterval);
         // Stops coding quiz and sends message.
         sendMessage();
+
     }
 
- }, 7500);
+ }, 1000);
 }
 
 // Function that sends message once timer reaches 0 seconds.
 function sendMessage() {
+    // Add function to concatenate score at the end of text
     timeEl.textContent = "Out of time! Your score is ";
+    var h1El = document.createElement("h1");
+    h1El.setAttribute("style", "background-color: red; color: #fff");
+    mainEl.appendChild(h1El);
+
 }
 
-// generateBtn.startQuiz.addEventListener("click", setTime);
-
+//----------
 
 var btn = document.querySelectorAll("button");
+var startQuiz = btn[0]
 
 // Styling 'Start Quiz' button.
-btn[0].setAttribute("style", "font-size: 18px; padding: 10px; background-color: #8906b1; color: #fff");
+startQuiz.setAttribute("style", "padding: 10px; border: none; font-size: 18px; background-color: #330582; color: #fff");
 
-var styleOl = document.querySelectorAll("li");
-// styleOl.style.color = "#8906b1";
+//----------
 
-for (var i = 0; i < styleOl.length; i++) {
-    styleOl[i].setAttribute("style", "color: #330582");
+var array1 = [
+    // Correct answer: alerts
+    "strings",
+    "boolean",
+    "alerts",
+    "numbers"
+];
+
+var ol = document.getElementById("first-ol");
+var p1 = document.getElementById("p1");
+
+// The variable startQuiz was created by selecting the first item in the 'button' index.
+startQuiz.addEventListener("click", function() {
+
+    
+    var h2El = document.createElement("h2");
+    h2El.innerText = "Commonly used date types DO NOT include:";
+    p1.append(h2El);
+
+    array1.forEach(function(event){
+        var li = document.createElement("li");
+        li.innerText = event;
+        ol.append(li);
+    })
+
+});
+
+// var liEl = document.querySelectorAll("li");
+// for (var i = 0; i < liEl.length; i++) {
+//     liEl[i].setAttribute("style", "color: #330582");
+// }
+
+
+
+//----------
+
+function correctBtn() {
+    // var right = "Correct!"
+    // var wrong = "Wrong"
+    var pTag = document.querySelectorAll(".p-ol");
+
+    for (var i = 0; i < pTag.length; i++);
+
+    if(data-choice === "Correct!") {
+        addEventListener("click", pTag[i])
+    }
+
 }
-
-var liEl = document.getElementById("ol");
-
-// var pTag = document.querySelectorAll(".p-ol");
 
 // Submit button after user inputs their initials.
 var submitEL = document.getElementById("submit-initials");
 
 function submitInitials(event) {
-    // Prevents default action
+    // Prevents default action of refreshing the page
     event.preventDefault();
     var inputedInitials = // user input
     submitEL.textContent = inputedInitials
 }
 
+// When user clicks the 'Start Quiz' button, it will call the set Time function and start a timer countdown.
+startQuiz.addEventListener("click", setTime);
 // When user clicks submit, they will be taken to the page of the highscores.
 submitEL.addEventListener("click", submitInitials);
 
